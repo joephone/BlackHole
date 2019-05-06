76528437d8202d6a381db741b3511f7a5cfe77a4
joephone <270136049@qq.com>
2019-05-06 12:10:44 +0800

    Initial commit

---
+++ app/src/main/java/com/transcendence/blackhole/base/app/AppAplication.java
@@ -0,0 +1,10 @@
+package com.transcendence.blackhole.base.app;
+
+/**
+ * @author Joephone on 2019/5/6 11:49
+ * @E-Mail Address：joephonechen@gmail.com
+ * @Desc
+ */
+
+public class AppAplication extends LibApplication {
+}
---
+++ library/src/main/java/com/transcendence/blackhole/base/app/LibApplication.java
@@ -0,0 +1,13 @@
+package com.transcendence.blackhole.base.app;
+
+import android.app.Application;
+
+/**
+ * @author Joephone on 2019/5/6 11:42
+ * @E-Mail Address：joephonechen@gmail.com
+ * @Desc 全局
+ */
+
+public class LibApplication extends Application {
+
+}
