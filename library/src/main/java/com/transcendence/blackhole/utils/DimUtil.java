7e9db20ea0c1f944a3eed15c25d704852b61a637
joephone <270136049@qq.com>
2019-09-20 18:00:06 +0800

    sth

---
+++ library/src/main/java/com/transcendence/blackhole/utils/DimUtil.java
@@ -0,0 +1,42 @@
+package com.transcendence.blackhole.utils;
+
+import android.content.res.Resources;
+
+/**
+ * px 和dip之间转换方法
+ * @author zhiwu_yan
+ * @version 1.0
+ * @since 2015-06-19  11:29
+ */
+public class DimUtil {
+
+    public static int getScreenWidth(){
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight(){
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    public static int px2Dip(int px){
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f);
+    }
+
+
+    public static float dip2Px(float dip){
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return  (dip * scale + 0.5f);
+    }
+
+
+    public static int px2Sp(float px) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (px / fontScale + 0.5f);
+    }
+
+    public static int sp2Px( float spValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
