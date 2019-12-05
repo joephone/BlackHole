85b440dbc9e2422dc8894d9ad518d16f1bc79c4b
joephone <270136049@qq.com>
2019-12-05 13:37:17 +0800

    gradle

--- wan/src/main/java/com/transcendence/wan/base/fragment/HomeFragment.java
+++ wan/src/main/java/com/transcendence/wan/base/fragment/HomeFragment.java
@@ -1,19 +1,24 @@
 package com.transcendence.wan.base.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.hjq.image.ImageLoader;
 import com.transcendence.blackhole.adapter.GoweiiFragmentPagerAdapter;
 import com.transcendence.blackhole.fragment.BlankFragment;
 import com.transcendence.wan.R;
 import com.transcendence.wan.main.WanMainActivity;
+
+import javax.xml.transform.Transformer;
 
 /**
  * @author Joephone on 2019/11/27 11:50
@@ -24,11 +29,12 @@ import com.transcendence.wan.main.WanMainActivity;
  */
 
 public class HomeFragment extends WanBaseFragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_SHOW_TEXT = "text";
 
     private ImageView ivLeft,ivRight;
-    private ViewPager mVp;
+//    private Banner
+    private RecyclerView mRv;
     private GoweiiFragmentPagerAdapter adapter;
 
 
@@ -37,8 +43,11 @@ public class HomeFragment extends WanBaseFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.fragment_home, container, false);
         initView(rootView);
+        createHeaderBanner();
         return rootView;
     }
+
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -46,7 +55,7 @@ public class HomeFragment extends WanBaseFragment {
     }
 
     private void initView(View rootView) {
-        mVp = rootView.findViewById(R.id.vp);
+        mRv = rootView.findViewById(R.id.rv);
         ivLeft = rootView.findViewById(R.id.ivLeft);
         ivRight = rootView.findViewById(R.id.ivRight);
         ivLeft.setOnClickListener(new View.OnClickListener() {
@@ -68,14 +77,36 @@ public class HomeFragment extends WanBaseFragment {
     }
 
 
-    private void initVP() {
-        //预加载
-        mVp.setOffscreenPageLimit(1);
-        adapter = new GoweiiFragmentPagerAdapter(getActivity().getSupportFragmentManager());
-        adapter.setTitles("广场","主页面");
-        adapter.setFragments(DamaFragment.newInstance("广场"), BlankFragment.newInstance("主页面"));
-        mVp.setAdapter(adapter);
-        mVp.setCurrentItem(1);
+    private void createHeaderBanner() {
+//        if (mBanner == null) {
+//            mBanner = new Banner(getContext());
+//            int height = (int) (DisplayInfoUtils.getInstance().getWidthPixels() * (9F / 16F));
+//            mBanner.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height));
+//            mBanner.setImageLoader(new com.youth.banner.loader.ImageLoader() {
+//                @Override
+//                public void displayImage(Context context, Object url, ImageView imageView) {
+//                    ImageLoader.banner(imageView, (String) url);
+//                }
+//            });
+//            mBanner.setIndicatorGravity(BannerConfig.CENTER);
+//            mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+//            mBanner.setBannerAnimation(Transformer.Default);
+//            mBanner.startAutoPlay();
+//            mBanner.setDelayTime(5000);
+//            mBanner.setOnBannerListener(new OnBannerListener() {
+//                @Override
+//                public void OnBannerClick(int position) {
+//                    BannerBean bean = mBannerBeans.get(position);
+//                    WebActivity.start(getContext(), bean.getTitle(), bean.getUrl());
+//                }
+//            });
+//            mAdapter.addHeaderView(mBanner, 0);
+//        }
+//        if (SettingUtils.getInstance().isShowBanner()) {
+//            mBanner.setVisibility(View.VISIBLE);
+//        } else {
+//            mBanner.setVisibility(View.GONE);
+//        }
     }
     /**
      * Use this factory method to create a new instance of
